e83844f4868c028cfd6dcebd247b9127ffc2feeb
ASaidOguz <ahmetsaid2136@gmail.com>
2022-11-19 21:58:43 +0300

    initial commit

---
+++ pages/api/getNftValidation.js
@@ -0,0 +1,31 @@
+import { ethers } from "ethers"
+
+const Wallet=new ethers.Wallet(process.env.WALLET_PRIVATE_KEY,
+ethers.getDefaultProvider(process.env.ALCHEMY_URL))
+const ValidationCollection="0x60576A64851C5B42e8c57E3E4A5cF3CF4eEb2ED6"
+export default async function handler(req, res) {
+  try {
+   if(req.method!='POST'){
+    return res.status(400).json({message:"Please use post request"})
+   }
+   const walletAddress=req.body.address
+   var requestOptions = {
+    method: 'GET',
+    
+  };
+   const response=await fetch(`${process.env.ALCHEMY_NFT_URL}/getNFTs/
+   ?owner=${walletAddress}&contractAddresses%5B%5D=${ValidationCollection}`,requestOptions)
+   .then(data => data.json())
+   if(response.ownedNfts.length!=0){
+    console.log(response.ownedNfts)
+    res.status(200).json({message:"Validation succesful!"})
+   }else{
+    alert("Validation unsuccesful!!")
+    res.status(400).json({message:"Validation unsuccesful!"})
+   
+   }
+  } catch (err) {
+    console.log(err)
+    res.status(500).json({message:"Internal Server Error!"})
+  }
+}
