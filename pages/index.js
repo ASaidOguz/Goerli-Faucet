0c6f03ab2d2925f91e182670401bcf10ae472223
ASaidOguz <ahmetsaid2136@gmail.com>
2022-11-22 15:23:35 +0300

    cleaned up some imports

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
-import { Button } from '@material-ui/core'
+
 import Head from 'next/head'
-import Image from 'next/image'
 import { useState } from 'react'
 import styles from '../styles/Home.module.css'
 
